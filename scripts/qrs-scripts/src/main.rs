7ade2a322d814e112611bc75490b18e2d0b914c9
sugaim <50139852+sugaim@users.noreply.github.com>
2024-02-11 12:11:15 +0900

    update ci.yml

--- scripts/qrs-scripts/src/main.rs
+++ scripts/qrs-scripts/src/main.rs
@@ -1,9 +1,6 @@
-#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
-
 pub mod gen_schema;
 pub mod utils;
 
-#[cfg_attr(coverage_nightly, coverage(off))]
 fn main() -> anyhow::Result<()> {
     env_logger::init();
     gen_schema::gen_schema()?;
